cc9452b9f4252027e10914111ee28d37ae485dfc
lasha <lashatatu@gmail.com>
2023-09-04 11:32:35 +0400

    193

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Main from "./Main";
 import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from './StartScreen';
 
 const initialState = {
   questions: [],
@@ -26,7 +29,9 @@ function reducer(state, action) {
 }
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions=questions.length
 
   useEffect(() => {
     fetch("http://localhost:9000/questions")
@@ -43,7 +48,11 @@ const App = () => {
   return (
     <div className={"app"}>
       <Header />
-      <Main></Main>
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions}/>}
+      </Main>
     </div>
   );
 };
